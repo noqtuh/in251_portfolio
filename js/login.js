6616d52ad532ddcbc4d0a0ac680349eaa30f3c9d
Noé Formanek <noe.formanek@gmail.com>
2026-05-27 19:20:38 +0200

    feat: added all the content to the CV.

--- js/login.js
+++ js/login.js
@@ -2,6 +2,5 @@
 
 function login() {
     const enteredPassword = document.getElementById("password").value;
-    console.log(enteredPassword);
     window.location.href = enteredPassword+".html";
 }
